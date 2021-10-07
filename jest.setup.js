e11ea511679ea379186274491de5f8eefea235a6
Eric Kim <erickim195@gmail.com>
2021-10-07 14:16:32 -0700

    Remove unnecessary semi-colon

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
 import { TextEncoder, TextDecoder } from 'util'
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
