b836d47f17c47d1a0557fab1c55f24840a613932
Eric Kim <erickim195@gmail.com>
2021-10-07 11:05:06 -0700

    Add ./api route

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,5 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import nc from 'next-connect'
+import { NextApiRequest, NextApiResponse } from 'next'
+import onError from '../../middleware/error'
 
-export default function handler(req, res) {
-  res.status(200).json({ message: './api' })
-}
+const handler = nc<NextApiRequest, NextApiResponse>({
+  onError
+})
+
+handler.get(async (req, res): Promise<void> => {
+  const docsUrl = "https://course-reqs.notion.site/course-reqs/Course-Reqs-560a1df7302a4a358fb14b7196b1d52d"
+
+  res.setHeader('Content-type', 'text/html')
+  res.send(`Welcome to the Course Reqs API! For more information, check out the detailed <a href="${docsUrl}">documentation</a>.`);
+})
+
+export default handler
