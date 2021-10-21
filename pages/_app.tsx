297ef01e913fa9a637d0a272ee7b03c540f0c9a1
Eric Kim <erickim195@gmail.com>
2021-10-21 00:40:14 -0700

    Add HTML head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import { ThemeProvider } from 'theme-ui'
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Course Reqs</title>
+        <meta name="description" content="Course requisite api and visualization" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   )
