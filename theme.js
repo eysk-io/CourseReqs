3918132080f60bd6383e648fe6b19b100ee8a5ae
Eric Kim <erickim195@gmail.com>
2021-10-21 00:31:57 -0700

    Add theme-ui with deep preset theme

---
+++ theme.js
@@ -0,0 +1,25 @@
+import { deep } from '@theme-ui/presets'
+
+const theme = {
+    ...deep,
+    containers: {
+        courseBox: {
+            boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+            border: '1px solid',
+            borderColor: 'muted',
+            borderRadius: '4px',
+            p: 2,
+        },
+        page: {
+            width: '100%',
+            maxWidth: '960px',
+            m: 0,
+            mx: 'auto',
+        }
+    },
+    styles: {
+        ...deep.styles
+    }
+}
+
+export default theme
