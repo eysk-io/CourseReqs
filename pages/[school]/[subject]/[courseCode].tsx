b3bf2512950c28d216fe6a13fe5a4b102632820e
Eric Kim <erickim195@gmail.com>
2021-10-22 00:05:48 -0700

    Add three.js example to courseCode page

--- pages/[school]/[subject]/[courseCode].tsx
+++ pages/[school]/[subject]/[courseCode].tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, createRef } from 'react'
 import { GetServerSideProps } from 'next'
+import * as THREE from 'three'
 import { connectToDB } from '../../../db/connect'
 import { getCourse } from '../../../db/course'
 import CourseBox from '../../../components/CourseBox'
@@ -7,6 +8,35 @@ import CourseBox from '../../../components/CourseBox'
 const CoursePage: FC<{ courseInfo?: any; }> = ({
     courseInfo 
 }) => {
+    const divRef = createRef()
+    useEffect(() => {
+        const scene = new THREE.Scene()
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        )
+        const renderer = new THREE.WebGLRenderer()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        const divRefNode = divRef.current as Node
+        divRefNode.appendChild(renderer.domElement)
+        const geometry = new THREE.BoxGeometry()
+        const material = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        })
+        const cube = new THREE.Mesh(geometry, material)
+        scene.add(cube)
+        camera.position.z = 5
+        function animate() {
+            requestAnimationFrame(animate)
+            cube.rotation.x += 0.01
+            cube.rotation.y += 0.01
+            renderer.render(scene, camera)
+        }
+        animate()
+    }, [divRef])
+
     const { 
         school, 
         subject, 
@@ -16,8 +46,10 @@ const CoursePage: FC<{ courseInfo?: any; }> = ({
         notes 
     } = courseInfo
     return (
-        <div className={
-            `${school.toLowerCase()}_${subject.toLowerCase()}_${code} course_page`}
+        <div 
+            className={`${school.toLowerCase()}_${subject.toLowerCase()}_${code} course_page`}
+            ref={divRef}
+            xmlns="http://www.w3.org/1999/xhtml"
         >
             <CourseBox 
                 school={school}
