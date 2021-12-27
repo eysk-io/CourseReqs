76cddb704caea3c065388031a1f0c53c77376df2
Eric Kim <erickim195@gmail.com>
2021-12-27 09:24:50 -0800

    Get back SSR

--- pages/[school]/[subject]/[courseCode].tsx
+++ pages/[school]/[subject]/[courseCode].tsx
@@ -1,82 +1,61 @@
-import React, { FC, useState, useEffect } from "react"
+import React, { FC } from "react"
 import { GetServerSideProps } from "next"
+import { connectToDB } from "../../../db/connect"
+import { getCourse } from "../../../db/course"
 import createNodes from "../../../utils/course_map/createNodes"
 import CourseInfo from "../../../components/CourseInfo"
 import CourseMap from "../../../components/CourseMap"
-import Spinner from "../../../components/Spinner"
 
-const CoursePage: FC<{
-    school?: string,
-    subject?: string,
-    code?: number
-}> = ({ school, subject, code }) => {
-    const [loading, setLoading] = useState(true)
-    const [title, setTitle] = useState("")
-    const [credits, setCredits] = useState("")
-    const [description, setDescription] = useState("")
-    const [notes, setNotes] = useState("")
-    const [nodes, setNodes] = useState([])
-    const [preRequisitesText, setPreRequisitesText] = useState("")
-    const [coRequisitesText, setCoRequisitesText] = useState("")
-    const [equivalenciesText, setEquivalenciesText] = useState("")
-
-    useEffect(() => {
-        const fetchCourseData = async () => {
-            let courseInfo: any = await fetch(`${process.env.COURSE_REQS_URL}/api/course/${school}/${subject}/${code}`)
-            courseInfo = await courseInfo.json()
-            courseInfo.data.nodes = []
-            createNodes(courseInfo.data)
-            setNodes(courseInfo.data.nodes)
-            setTitle(courseInfo.data.title)
-            setCredits(courseInfo.data.credits)
-            setDescription(courseInfo.data.description)
-            setNotes(courseInfo.data.notes)
-            setPreRequisitesText(courseInfo.data.preRequisitesText)
-            setCoRequisitesText(courseInfo.data.coRequisitesText)
-            setEquivalenciesText(courseInfo.data.equivalenciesText)
-            setLoading(false)
-        }
-        fetchCourseData()
-    }, [code, loading, school, subject, title])
+const CoursePage: FC<{ 
+    school?: any;
+    subject?: any;
+    code?: any;
+    title?: any;
+    credits?: any;
+    description?: any;
+    notes?: any;
+    nodes?: any;
+    preRequisitesText?: any;
+    coRequisitesText?: any;
+    equivalenciesText?: any;
+}> = ({ school, subject, code, title, credits, description, notes, nodes, preRequisitesText, coRequisitesText, equivalenciesText }) => {
 
     return (
-        <>
-            {loading ? (
-                <Spinner />
-            ) : (
-                <div className={
-                    `${school.toLowerCase()}_\
-                    ${subject.toLowerCase()}_\
-                    ${code}_page`
-                }>
-                    <CourseInfo 
-                        school={school.toUpperCase()}
-                        subject={subject.toUpperCase()}
-                        code={code}
-                        credits={credits}
-                        title={title}
-                        description={description}
-                        notes={notes}
-                        preRequisitesText={preRequisitesText}
-                        coRequisitesText={coRequisitesText}
-                        equivalenciesText={equivalenciesText}
-                        />
-                    <CourseMap nodes={nodes}/>
-                </div>
-            )}
-        </>
+        <div className={
+            `${school.toLowerCase()}_\
+            ${subject.toLowerCase()}_\
+            ${code}_page`
+        }>
+            <CourseInfo 
+                school={school}
+                subject={subject}
+                code={code}
+                credits={credits}
+                title={title}
+                description={description}
+                notes={notes}
+                preRequisitesText={preRequisitesText}
+                coRequisitesText={coRequisitesText}
+                equivalenciesText={equivalenciesText}
+            />
+            <CourseMap nodes={nodes}/>
+        </div>
     )
 }
 
 export default CoursePage
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+    await connectToDB()
+    let courseInfo = { nodes: [] }
     const { school, subject, courseCode } = context.query
-    return { 
-        props: {
-            school: school,
-            subject: subject,
-            code: courseCode
-        } 
+    if (
+        typeof school === "string" &&
+        typeof subject === "string" &&
+        typeof courseCode === "string"
+    ) {
+        courseInfo = await getCourse(school, subject, courseCode)
     }
+    createNodes(courseInfo)
+    return { props: courseInfo }
 }
