26df0aac1bd9c54a4cd5b5c64a203c4386283f54
Eric Kim <erickim195@gmail.com>
2022-01-01 14:51:39 -0800

    Create draft of homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,37 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
-import { useRouter } from 'next/router'
+import CourseSelector from '../components/CourseSelector'
 
 export default function Home() {
-  const router = useRouter()
-  const inputs = ["school", "subject", "code"]
+  const [allSubjects, setAllSubjects] = useState([])
+  const [loading, setLoading] = useState(true)
 
-  const capitalizeFirstLetter = (s: string): string => {
-    return s.charAt(0).toUpperCase() + s.slice(1);
+  useEffect(() => {
+    const fetchSubjectData = async () => {
+      let subjectsInfo: any = await fetch(`${process.env.COURSE_REQS_URL}/api/course/ubc`)
+      subjectsInfo = await subjectsInfo.json()
+      setAllSubjects(subjectsInfo.data.subjects)
+      setLoading(false)
   }
-
-  const handleSubmit = (e): void => {
-    e.preventDefault()
-    const school = e.target.elements.school.value
-    const subject = e.target.elements.subject.value
-    const code = e.target.elements.code.value
-    router.push(`/${school}/${subject}/${code}`)
-  }
+  fetchSubjectData()
+  }, [])
 
   return (
-    <div className={styles.container}>
-      <h1>Home Page</h1>
-      <form onSubmit={e => handleSubmit(e)}>
-        {
-          inputs.map((input, index) => (
-              <div key={index}>
-                <label 
-                  key={index + 10}
-                  htmlFor={input}
-                >
-                  {capitalizeFirstLetter(input)}: 
-                </label>
-                <input 
-                  key={index + 20}
-                  type="text" 
-                  id={input} 
-                  name={input} 
-                />
-              </div>
-            ))
+    <>
+      {loading ? (
+            <div>Loading</div>
+          ) : (
+            <div className={styles.container}>
+              <h1>Pick your course below:</h1>
+              <CourseSelector 
+                school={"UBC"}
+                subject={"ADHE"}
+                code={"313"}
+                subjectCourses={allSubjects}
+              />
+            </div>
+          )
         }
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    </>
   )
 }
