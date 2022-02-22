ad9caf3b896a2a63c8e015c06bcf75fa9ffe83e1
Eric Kim <erickim195@gmail.com>
2022-02-21 18:08:42 -0800

    Add ability to report issues as an issue to the repo

--- pages/report-a-bug/index.tsx
+++ pages/report-a-bug/index.tsx
@@ -1,10 +1,43 @@
-import { FC } from 'react'
+import { GetServerSideProps } from 'next'
+import { FC, useState } from 'react'
 
-const ReportABug: FC<{}> = () => {
+const ReportABug: FC<{
+    authToken?: string
+}> = ({ authToken }) => {
+    const [subject, setSubject] = useState("")
+    const [code, setCode] = useState("")
+    const [bug, setBug] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const url = "https://api.github.com/repos/eyskim/CourseReqs/issues"
+
+        let xhr = new XMLHttpRequest()
+        xhr.open("POST", url)
+
+        xhr.setRequestHeader(
+            "Authorization",
+            `token ${authToken}`
+        )
+
+        xhr.setRequestHeader(
+            "Content-Type",
+            "application/json;charset=UTF-8"
+        )
+        
+        let data = {
+            "title": "Bug Report",
+            "body": `Course: ${subject} ${code}\n ${bug}`,
+            "labels": ["bug"]
+        }
+
+        xhr.send(JSON.stringify(data))
+    }
+
     return (
         <div className="report-a-bug">
             <h1>Report a bug:</h1>
-            <form id="bug-form">
+            <form id="bug-form" onSubmit={handleSubmit}>
                 <label htmlFor="course-subject">
                     Course Subject (or leave blank):
                 </label><br/>
@@ -12,6 +45,7 @@ const ReportABug: FC<{}> = () => {
                     type="text" 
                     id="course-subject" 
                     name="course-subject"
+                    onChange={e => setSubject(e.target.value)}
                 /><br/>
                 <label htmlFor="course-code">
                     Course Code (or leave blank):
@@ -20,6 +54,7 @@ const ReportABug: FC<{}> = () => {
                     type="text"
                     id="course-code"
                     name="course-code"
+                    onChange={e => setCode(e.target.value)}
                 /><br/>
                 <label htmlFor="bug-form">
                     Please provide some information about the bug below:
@@ -29,6 +64,7 @@ const ReportABug: FC<{}> = () => {
                     rows={4}
                     cols={50}
                     name="bug-info"
+                    onChange={e => setBug(e.target.value)}
                 /><br/>
                 <input type="submit"/>
             </form>
@@ -37,3 +73,11 @@ const ReportABug: FC<{}> = () => {
 }
 
 export default ReportABug
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    return { 
+        props: { 
+            authToken: process.env.COURSE_REQS_BUG_GITHUB_TOKEN
+        }
+    }
+}
