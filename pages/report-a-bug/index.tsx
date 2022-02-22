f9a50e4a4385f81e9ddea90e6f5d99156094c0f9
Eric Kim <erickim195@gmail.com>
2022-02-21 17:28:47 -0800

    Make formatting changes to code

--- pages/report-a-bug/index.tsx
+++ pages/report-a-bug/index.tsx
@@ -5,12 +5,31 @@ const ReportABug: FC<{}> = () => {
         <div className="report-a-bug">
             <h1>Report a bug:</h1>
             <form id="bug-form">
-                <label htmlFor="course-subject">Course Subject (or leave blank):</label><br/>
-                <input type="text" id="course-subject" name="course-subject"/><br/>
-                <label htmlFor="course-code">Course Code (or leave blank):</label><br/>
-                <input type="text" id="course-code" name="course-code"/><br/>
-                <label htmlFor="bug-form">Please provide some information about the bug below:</label><br/>
-                <textarea form="bug-form" rows={4} cols={50} name="bug-info"/><br/>
+                <label htmlFor="course-subject">
+                    Course Subject (or leave blank):
+                </label><br/>
+                <input 
+                    type="text" 
+                    id="course-subject" 
+                    name="course-subject"
+                /><br/>
+                <label htmlFor="course-code">
+                    Course Code (or leave blank):
+                </label><br/>
+                <input
+                    type="text"
+                    id="course-code"
+                    name="course-code"
+                /><br/>
+                <label htmlFor="bug-form">
+                    Please provide some information about the bug below:
+                </label><br/>
+                <textarea 
+                    form="bug-form"
+                    rows={4}
+                    cols={50}
+                    name="bug-info"
+                /><br/>
                 <input type="submit"/>
             </form>
         </div>
