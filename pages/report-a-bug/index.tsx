649a2235a62f2512cd69d876b77c20fd8c02aec6
Eric Kim <erickim195@gmail.com>
2022-02-21 16:20:47 -0800

    Create basic form to report bugs

--- pages/report-a-bug/index.tsx
+++ pages/report-a-bug/index.tsx
@@ -1,7 +1,20 @@
 import { FC } from 'react'
 
 const ReportABug: FC<{}> = () => {
-    return <div>ReportABug Page</div>
+    return (
+        <div className="report-a-bug">
+            <h1>Report a bug:</h1>
+            <form id="bug-form">
+                <label htmlFor="course-subject">Course Subject (or leave blank):</label><br/>
+                <input type="text" id="course-subject" name="course-subject"/><br/>
+                <label htmlFor="course-code">Course Code (or leave blank):</label><br/>
+                <input type="text" id="course-code" name="course-code"/><br/>
+                <label htmlFor="bug-form">Please provide some information about the bug below:</label><br/>
+                <textarea form="bug-form" rows={4} cols={50} name="bug-info"/><br/>
+                <input type="submit"/>
+            </form>
+        </div>
+    )
 }
 
 export default ReportABug
